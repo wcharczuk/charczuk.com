5b2ae190b6dd0e5ce3f14dfdf41269d87b84353e
Will Charczuk <will@blend.com>
2018-11-23 23:50:23 -0800

    updates

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,9 +4,25 @@
 
 		if (e.keyCode == 37) {
 			$(".nav-link-previous").click();
-		}
-		if (e.keyCode == 39) {
+		} else if (e.keyCode == 39) {
 			$(".nav-link-next").click();
+		} else if (e.keyCode == 27) {
+			$(".lightbox").addClass("hidden");
 		}
 	});
 })($);
+
+(function ($) {
+	$(".post-image").on('click', function (e) {
+		e.preventDefault();
+		$(".lightbox").removeClass("hidden");
+	});
+	$(".lightbox-close").on('click', function (e) {
+		e.preventDefault();
+		$(".lightbox").addClass("hidden");
+	});
+	$(".lightbox").on('click', function (e) {
+		e.preventDefault();
+		$(".lightbox").addClass("hidden");
+	});
+})($)
